403a963c0b37afacd5bcc0c2eff181ce304e9793
Vasu Patel <95951843+VasuPatel5541@users.noreply.github.com>
2024-01-21 05:38:19 +0000

    datatypes

---
+++ 01_basics/dtypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all Js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+let name ="Vasu"
+let age = 19
+let isLoggedIn = false
+// PRIMITIVE DATATYPES
+//number =>2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol =>unique
+
+// object
+
+console.log(typeof age)
