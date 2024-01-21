149bf4c943e06d076b6e23851119c8dfcc91a055
Vasu Patel <95951843+VasuPatel5541@users.noreply.github.com>
2024-01-21 05:18:15 +0000

    container config done

---
+++ 01_basics/var.js
@@ -0,0 +1,13 @@
+const accId = 1455
+let accEmail = "hello@gmail.com"
+var accPassword = "12345"
+accCity = "jaipur"
+let accState
+
+// accId = 10
+accEmail = "world@gmail.com"
+accPassword = "09876"
+accCity = "Gujarat"
+
+console.log(accId)
+console.table([accId,accEmail,accPassword,accCity,accState])
